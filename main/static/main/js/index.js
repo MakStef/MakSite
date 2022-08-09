c2c78d2e514462b440da267f07385ed12ab873db
MakStef <stevaniwwasil@gmail.com>
2022-08-09 21:14:37 +0200

    Main Index smile face and mood rate fix and extend

--- main/static/main/js/index.js
+++ main/static/main/js/index.js
@@ -1,3 +1,19 @@
 // Changes face when button is hovered
 const buttonToSmile = document.querySelector(".to-smile")
-const facesContainer = document.querySelector(".welcome__face")
+const facesContainer = document.querySelector(".welcome__smile")
+let isFirst = true
+buttonToSmile.onmouseover = () => {
+    if (isFirst) {
+        facesContainer.firstElementChild.classList.remove("fa-arrow-down")
+        facesContainer.firstElementChild.classList.add("fa-arrow-up")
+        isFirst = false
+    }
+    else {
+        setTimeout(()=>{
+            facesContainer.firstElementChild.classList.remove("fa-arrow-up")
+            facesContainer.firstElementChild.classList.add("fa-arrow-down")
+            isFirst = true
+            }, 14000
+        )
+    }
+}
